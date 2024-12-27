48a39f554bc694be9a162a69b54c073486850c5b
Aaryan Raj <121605593+aaryanraj25@users.noreply.github.com>
2024-12-27 12:11:23 +0530

    initial

--- app/config.py
+++ app/config.py
@@ -1,9 +1,21 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+from pydantic import Field
+from pathlib import Path
+
+# Define a path
+template_folder = Path("path/to/templates")
+
+# Check if it's a directory
+if template_folder.is_dir():
+    print("It's a valid directory.")
+else:
+    print("Not a directory.")
+
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Meetyfi"
-    MONGODB_URL: str
+    MONGODB_URL: str 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
@@ -12,7 +24,11 @@ class Settings(BaseSettings):
     SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASSWORD: str
-
+    MAIL_STARTTLS: bool = Field(default=True)
+    MAIL_SSL_TLS: bool = Field(default=False)
+    TEMPLATE_FOLDER: Path = Field(
+        default=Path("C:/Users/Sakshi Sharma/meetyfi-backend/app/core/email_templates")
+    )
     class Config:
         env_file = ".env"
 
